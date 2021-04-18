45136b6941c283a757f1778429940d553296dea5
lawyiu <lawyiu.dev@gmail.com>
2021-04-17 20:30:27 -0700

    Add run dependency on finding the text outputs

--- web/preRun.js
+++ web/preRun.js
@@ -1,3 +1,16 @@
 Module["preRun"].push(function() {
     "use strict";
+    Module["addRunDependency"]("find_text_outputs");
+
+    function findParentElms() {
+        Module.serialOutElm = parent.document.getElementById('serial-console');
+        Module.errorOutElm = parent.document.getElementById('error-console');
+        Module["removeRunDependency"]("find_text_outputs");
+    }
+
+    if (/complete|interactive|loaded/.test(parent.document.readyState)) {
+        findParentElms();
+    } else {
+        parent.document.addEventListener('DOMContentLoaded', findParentElms);
+    }
 });
