dda12c105e7fcc2114f78d96ec6cbbb3bf532249
lawyiu <lawyiu.dev@gmail.com>
2021-04-19 23:43:58 -0700

    Add a welcome example program

---
+++ arduino_examples/welcome.ino
@@ -0,0 +1,12 @@
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println("Welcome to the 2D Robot Simulator!");
+    Serial.println("Please try selecting a different program to compile from the dropdown menu below.");
+    Serial.println("Be sure to click on the canvas and press the R (Restart simulation) or L (Load code) key to run the new code after pressing the compile button.");
+    Serial.println("Feel free to try running your own programs to control the robot!");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
