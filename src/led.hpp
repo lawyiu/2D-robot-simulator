73de9cb7a225f8b049f96a157aa704e5a872e772
lawyiu <lawyiu.dev@gmail.com>
2021-04-22 22:05:31 -0700

    Add another LED constructor with default color only

--- src/led.hpp
+++ src/led.hpp
@@ -9,6 +9,8 @@ class Led : public Output {
 public:
     Led(Robot& robot) : Led(robot, {13}, glm::vec2(0.0f, 0.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)) {}
     Led(Robot& robot, glm::vec2 position) : Led(robot, {13}, position, glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)) {}
+    Led(Robot& robot, std::initializer_list<uint32_t> pins, glm::vec2 position)
+        : Led(robot, pins, position, glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)) {}
     Led(Robot& robot, std::initializer_list<uint32_t> pins, glm::vec2 position, glm::vec4 color);
 
     void update() override;
