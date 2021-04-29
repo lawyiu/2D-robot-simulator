16b6b13772b1ababb4ba175eaa4dca3588843810
lawyiu <lawyiu.dev@gmail.com>
2021-04-28 22:31:50 -0700

    Allow panning the camera with arrow keys

--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,23 @@
 #include "app.hpp"
 
+#include <limits.h>
+
 App& App::getInstance() {
     static App app;
     return app;
+}
+
+void App::updateDeltaTime() {
+    int newMillis = millis();
+    mDeltaTimeMillis = newMillis - mPrevMillis;
+
+    // Handle overflow
+    if (mDeltaTimeMillis < 0) {
+        int leftOver = std::numeric_limits<int>::max() - mPrevMillis;
+        mDeltaTimeMillis = newMillis + leftOver;
+    }
+
+    mPrevMillis = newMillis;
 }
 
 void App::setup() {
@@ -13,6 +28,9 @@ void App::setup() {
 }
 
 void App::update() {
+    updateDeltaTime();
+    float dt = getDeltaTimeSeconds();
+
     if (mReload) {
         mRobot->reloadCode();
         mReload = false;
@@ -24,6 +42,28 @@ void App::update() {
 
     if (!mPause) {
         mRobot->update();
+    }
+
+    float keyPanDist = pixelsPerSecond * dt;
+    switch (mKeyPanState) {
+        case PAN_UP:
+            mOffsetY -= keyPanDist;
+            break;
+
+        case PAN_DOWN:
+            mOffsetY += keyPanDist;
+            break;
+
+        case PAN_LEFT:
+            mOffsetX -= keyPanDist;
+            break;
+
+        case PAN_RIGHT:
+            mOffsetX += keyPanDist;
+            break;
+
+        default:
+            break;
     }
 }
 
@@ -38,7 +78,7 @@ void App::draw(piksel::Graphics& g) {
         g.text("Paused", mTextPadding, height - mTextPadding);
     }
 
-    g.translate(width / 2.0f, height / 2.0f);
+    g.translate(width / 2.0f + mOffsetX, height / 2.0f + mOffsetY);
     g.scale(mScaleFactor, mScaleFactor);
 
     mRobot->draw(g);
@@ -64,6 +104,36 @@ void App::keyPressed(int key) {
 
         case GLFW_KEY_R:
             mRestart = true;
+            break;
+
+        case GLFW_KEY_UP:
+            mKeyPanState = PAN_UP;
+            break;
+
+        case GLFW_KEY_DOWN:
+            mKeyPanState = PAN_DOWN;
+            break;
+
+        case GLFW_KEY_LEFT:
+            mKeyPanState = PAN_LEFT;
+            break;
+
+        case GLFW_KEY_RIGHT:
+            mKeyPanState = PAN_RIGHT;
+            break;
+
+        default:
+            break;
+    }
+}
+
+void App::keyReleased(int key) {
+    switch (key) {
+        case GLFW_KEY_UP:
+        case GLFW_KEY_DOWN:
+        case GLFW_KEY_LEFT:
+        case GLFW_KEY_RIGHT:
+            mKeyPanState = PAN_NONE;
             break;
 
         default:
--- src/app.hpp
+++ src/app.hpp
@@ -12,6 +12,9 @@ class App : public piksel::BaseApp {
 public:
     static App& getInstance();
 
+    int getDeltaTimeMillis() { return mDeltaTimeMillis; }
+    float getDeltaTimeSeconds() { return mDeltaTimeMillis / 1000.0f; }
+
     void setup();
     void update();
     void draw(piksel::Graphics& g);
@@ -19,12 +22,28 @@ public:
     Robot& getCurrentRobot();
 
     void keyPressed(int key);
+    void keyReleased(int key);
     void mouseWheel(int delta);
 
 private:
     App() : piksel::BaseApp(640, 480) {}
     App(const App& other) {}
     ~App() {}
+
+    void updateDeltaTime();
+
+    int mPrevMillis = 0;
+    int mDeltaTimeMillis = 0;
+
+    enum KeyPanState {
+        PAN_NONE,
+        PAN_UP,
+        PAN_DOWN,
+        PAN_LEFT,
+        PAN_RIGHT
+    };
+
+    KeyPanState mKeyPanState = PAN_NONE;
 
     bool mPause = false;
     bool mReload = false;
@@ -36,6 +55,10 @@ private:
 
     float mScaleFactor = INIT_SCALE_FACTOR; // Pixels per cm
     float mWheelFactor = 100.0f;            // Controls mouse wheel speed
+
+    float pixelsPerSecond = 100.0f;
+    float mOffsetX = 0.0f;
+    float mOffsetY = 0.0f;
 
     std::unique_ptr<Robot> mRobot;
 };
