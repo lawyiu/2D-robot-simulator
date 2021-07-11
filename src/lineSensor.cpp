b1bad1cd352d44d2371b994df7dc057e4975f26d
lawyiu <lawyiu.dev@gmail.com>
2021-07-11 00:38:43 -0700

    Add line sensor

---
+++ src/lineSensor.cpp
@@ -0,0 +1,66 @@
+#include "lineSensor.hpp"
+#include "pinsController.hpp"
+#include "robot.hpp"
+
+using namespace piksel;
+
+LineSensor::LineSensor(Robot& robot, uint32_t pin, glm::vec2 position)
+    : Input(robot), mPinsCtrl(robot.getPinsController()), mPosition(position) {
+    mWorld = robot.getWorld();
+    createBody();
+    createJoint();
+}
+
+LineSensor::~LineSensor() {
+    mWorld->DestroyJoint(mWeldJoint);
+    mWorld->DestroyBody(mBody);
+}
+
+void LineSensor::createBody() {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(mPosition.x, mPosition.y);
+    mBody = mWorld->CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(mWidth / 2.0f, mHeight / 2.0f);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.isSensor = true;
+
+    mBody->CreateFixture(&fixtureDef);
+}
+
+void LineSensor::createJoint() {
+    // Attach to robot body
+    b2Body* robotBody = mRobot.getBody();
+    b2WeldJointDef weldJointDef;
+    b2Vec2 position(mPosition.x, mPosition.y);
+    weldJointDef.Initialize(mBody, robotBody, position);
+    weldJointDef.collideConnected = false;
+
+    mWeldJoint = mWorld->CreateJoint(&weldJointDef);
+}
+
+void LineSensor::setState(bool state) {
+    mPinsCtrl.setPinValue(mPinNum, state, ValueType::DIGITAL);
+}
+
+void LineSensor::update() {
+}
+
+void LineSensor::draw(Graphics& g) {
+    g.push();
+
+    g.stroke(mColor);
+    g.noFill();
+    g.rectMode(DrawMode::CENTER);
+
+    b2Vec2 sensorPos = mBody->GetPosition();
+    g.translate(sensorPos.x, sensorPos.y);
+    g.rotate(mBody->GetAngle());
+    g.rect(0.0f, 0.0f, mWidth, mHeight);
+
+    g.pop();
+}
---
+++ src/lineSensor.hpp
@@ -0,0 +1,42 @@
+#ifndef LINE_SENSOR_HPP
+#define LINE_SENSOR_HPP
+
+#include "input.hpp"
+
+#include <glm/glm.hpp>
+
+class PinsController;
+class Pin;
+class b2World;
+class b2Body;
+class b2Joint;
+
+class LineSensor : public Input {
+public:
+    // Position is relative to origin of robot
+    LineSensor(Robot& robot, uint32_t pin, glm::vec2 position);
+    ~LineSensor();
+
+    // Used to set if the line sensor is over a line or not
+    void setState(bool state);
+
+    void update();
+    void draw(piksel::Graphics& g);
+
+private:
+    void createBody();
+    void createJoint();
+
+    b2World* mWorld;
+    b2Body* mBody;
+    b2Joint* mWeldJoint;
+    PinsController& mPinsCtrl;
+    uint32_t mPinNum;
+    glm::vec2 mPosition;
+
+    const glm::vec4 mColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    const float mWidth = 0.01f;  // 1 CM
+    const float mHeight = 0.01f; // 1 CM
+};
+
+#endif /* LINE_SENSOR_HPP */
