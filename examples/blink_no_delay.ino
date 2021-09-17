12a00944467d33b004aae4378a34f67d8fdf53d7
lawyiu <lawyiu@users.noreply.github.com>
2021-09-17 23:29:46 +0000

    Built artifacts of ed74298 [ci skip]

---
+++ examples/blink_no_delay.ino
@@ -0,0 +1,28 @@
+#define LED_PIN 13
+
+bool on = false;
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void toggle() {
+    if (on) {
+        digitalWrite(LED_PIN, LOW);
+    } else {
+        digitalWrite(LED_PIN, HIGH);
+    }
+
+    on = !on;
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    static unsigned long prevMillis = 0;
+    unsigned long currMillis = millis();
+    if (currMillis - prevMillis > 1000) {
+        prevMillis = currMillis;
+        toggle();
+    }
+}
